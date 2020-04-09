d24d3d35ba90ab78d82fdc5d99bac9ce6e5522d9
Andres <andtruj23@gmail.com>
2020-04-08 19:34:09 -0500

    Learning printf and print

---
+++ Python/Book_Exercises/Python3_The_Hard_Way/printf.py
@@ -0,0 +1,11 @@
+my_name = 'Andres'
+my_age= 33
+my_height=68    #inches
+my_weight= 250 #pounds
+my_eyes= 'brown'
+
+printf{f"My name is.. {my_name}."}
+printf{f"I'm {my_height} inches tall."}
+print{"Which is actually not very tall."}
+printf{f"I also have {my_eyes}  eyes!"}
+printf{f"Printf is neat! I can even do arithmetic..like  1+1={1+1}"}
