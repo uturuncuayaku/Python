9569a71f5b76a139fdcde8ebd0aaca3feb6964d7
Andres <andtruj23@gmail.com>
2020-04-11 16:23:46 -0500

    Python classes start Monday

--- Python/Book_Exercises/Python3_The_Hard_Way/printf.py
+++ Python/Book_Exercises/Python3_The_Hard_Way/printf.py
@@ -9,3 +9,15 @@ printf{f"I'm {my_height} inches tall."}
 print{"Which is actually not very tall."}
 printf{f"I also have {my_eyes}  eyes!"}
 printf{f"Printf is neat! I can even do arithmetic..like  1+1={1+1}"}
+print{"Print is very powerful as well!"}
+
+q = 1001
+p = 0.111
+print(q,p,p*q)
+# Output - 1001 .111 1001.111
+
+print(q,p,p*q, sep=",")
+# Output - 1001,.111,1001.111
+
+print(str(q) + " " + str(p) + " " + str(p * q))
+# Output - 1001 .111 1001.111
